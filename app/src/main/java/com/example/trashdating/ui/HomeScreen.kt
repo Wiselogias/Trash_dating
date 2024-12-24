00b979200f57264f3e9ca337aa9f39501dafc94c
Maksim <367485@niuitmo.ru>
2024-12-24 19:03:47 +0300

    add matches page with simple design

--- app/src/main/java/com/example/trashdating/ui/HomeScreen.kt
+++ app/src/main/java/com/example/trashdating/ui/HomeScreen.kt
@@ -37,10 +37,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.trashdating.R
 import com.example.trashdating.ui.theme.TrashDatingTheme
 
@@ -74,7 +72,8 @@ private fun Header() {
             .padding(top = 32.dp, start = 16.dp, end = 16.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = "ИСКРА", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Icon(painter = painterResource(R.drawable.logo), contentDescription = null)
+        Icon(painter = painterResource(R.drawable.app_name), contentDescription = null)
         Icon(imageVector = Icons.Default.Notifications, contentDescription = "Notifications")
     }
 }
