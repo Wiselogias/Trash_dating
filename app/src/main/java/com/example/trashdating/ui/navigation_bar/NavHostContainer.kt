44f0e24f73e4debdac87937c3e85f4df2baf2199
Maksim <367485@niuitmo.ru>
2024-12-24 17:50:09 +0300

    add simple home page

--- app/src/main/java/com/example/trashdating/ui/navigation_bar/NavHostContainer.kt
+++ app/src/main/java/com/example/trashdating/ui/navigation_bar/NavHostContainer.kt
@@ -11,6 +11,7 @@ import com.example.trashdating.ui.ChatScreen
 import com.example.trashdating.ui.ExploreScreen
 import com.example.trashdating.ui.HomeScreen
 import com.example.trashdating.ui.MatchesScreen
+import com.example.trashdating.ui.Profile
 import com.example.trashdating.ui.ProfileScreen
 
 @Composable
@@ -33,7 +34,13 @@ fun NavHostContainer(
             }
 
             composable("home") {
-                HomeScreen()
+                HomeScreen(
+                    onFollowedPeopleClick = {},
+                    onCreateStoryClick = {},
+                    onRelationshipTypeChanged = {},
+                    followed = listOf(Profile("Clare", "clare@gmail.com", "Pup")),
+                    profiles = listOf(Profile("Alice", "alice@example.com", "Profile picture"))
+                )
             }
 
             composable("matches") {
