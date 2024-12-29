ada3c0a224cf93292ee5a7a76c754e9d335b448d
Nikita Tarasevich <nikitat03@gmail.com>
2024-12-29 21:31:37 +0300

    profile photo

--- app/src/main/java/com/example/trashdating/ui/navigation_bar/BottomNavigationScreen.kt
+++ app/src/main/java/com/example/trashdating/ui/navigation_bar/BottomNavigationScreen.kt
@@ -1,5 +1,8 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -7,11 +10,17 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import coil.compose.rememberAsyncImagePainter
+import com.example.trashdating.ui.navigation_bar.IconNavItem
+import com.example.trashdating.ui.navigation_bar.ImageNavItem
 import com.example.trashdating.ui.navigation_bar.NavigationConstants
 
 @Composable
@@ -30,30 +39,64 @@ fun BottomBar(
         val currentRoute = navBackStackEntry?.destination?.route
 
         screens.forEach { screen ->
-            NavigationBarItem(
-                icon = { screen.icon() },
-                modifier = if (screen.route == "profile") {
-                    Modifier.size(66.dp)
-                } else {
-                    Modifier.size(46.dp)
-                },
-                selected = currentRoute == screen.route,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = MaterialTheme.colorScheme.surface,
-                    unselectedIconColor = Color(0xFFFF9401),
-                    indicatorColor = Color(0xFFFF9401),
+            when (screen) {
+                is IconNavItem -> {
+                    NavigationBarItem(
+                        icon = {
+                            Icon(
+                                painter = painterResource(screen.icon),
+                                contentDescription = ""
+                            )
+                        },
+                        modifier = Modifier.size(46.dp),
+                        selected = currentRoute == screen.route,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.surface,
+                            unselectedIconColor = Color(0xFFFF9401),
+                            indicatorColor = Color(0xFFFF9401),
+                        )
+                    )
+                }
 
-                )
-            )
+                is ImageNavItem -> {
+                    NavigationBarItem(
+                        icon = {
+                            Image(
+                                painter = rememberAsyncImagePainter(model = screen.imageUrl),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(66.dp)
+                                    .clip(CircleShape)
+                            )
+                        },
+                        selected = currentRoute == screen.route,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.surface,
+                            unselectedIconColor = Color(0xFFFF9401),
+                            indicatorColor = Color(0xFFFF9401),
+                        )
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/trashdating/ui/navigation_bar/NavigationItems.kt
+++ app/src/main/java/com/example/trashdating/ui/navigation_bar/NavigationItems.kt
@@ -2,30 +2,37 @@ package com.example.trashdating.ui.navigation_bar
 
 import com.example.trashdating.R
 
-data class BottomNavItem(
+sealed class BottomNavItem(open val route: String)
+
+data class IconNavItem(
     val icon: Int,
-    val route: String,
-)
+    override val route : String
+) : BottomNavItem(route)
+
+data class ImageNavItem(
+    val imageUrl: String,
+    override val route: String
+) : BottomNavItem(route)
 
 object NavigationConstants {
     val BottomNavItems = listOf(
-        BottomNavItem(
+        IconNavItem(
             icon = R.drawable.ic_home_24,
             route = "home"
         ),
-        BottomNavItem(
+        IconNavItem(
             icon = R.drawable.ic_search_24,
             route = "explore"
         ),
-        BottomNavItem(
-            icon = R.drawable.ic_group_24,
+        ImageNavItem(
+            imageUrl = "https://sun9-50.userapi.com/impg/og_hN2eEY4_hEZayQS9hbVWbHz3gRdJwSGxTtQ/GAqDztiBJps.jpg?size=444x592&quality=95&sign=b555184b7a55d7809c21274460bb407c&type=album",
             route = "profile"
         ),
-        BottomNavItem(
+        IconNavItem(
             icon = R.drawable.ic_chat_24,
             route = "chat"
         ),
-        BottomNavItem(
+        IconNavItem(
             icon = R.drawable.ic_group_24,
             route = "matches"
         )
