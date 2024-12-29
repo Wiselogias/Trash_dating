c93a772987305c58b893ecb583fd2d2be9335e22
Nikita Tarasevich <nikitat03@gmail.com>
2024-12-29 19:59:28 +0300

    revert

--- app/src/main/java/com/example/trashdating/ui/navigation_bar/NavigationItems.kt
+++ app/src/main/java/com/example/trashdating/ui/navigation_bar/NavigationItems.kt
@@ -1,74 +1,32 @@
 package com.example.trashdating.ui.navigation_bar
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import com.example.trashdating.R
 
 data class BottomNavItem(
-    val icon: @Composable (() -> Unit),
+    val icon: Int,
     val route: String,
 )
 
 object NavigationConstants {
     val BottomNavItems = listOf(
         BottomNavItem(
-            icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_home_24),
-                    contentDescription = "Home"
-                )
-            },
+            icon = R.drawable.ic_home_24,
             route = "home"
         ),
         BottomNavItem(
-            icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_search_24),
-                    contentDescription = "Explore"
-                )
-            },
+            icon = R.drawable.ic_search_24,
             route = "explore"
         ),
         BottomNavItem(
-            icon = {
-                Image(
-                    painter = painterResource(id = R.drawable.profile_photo),
-                    contentDescription = "Profile",
-                    modifier = Modifier
-                        .size(48.dp)
-                        .clip(CircleShape)
-                        .border(2.dp, Color(0xFFFF9401), CircleShape),
-                    contentScale = ContentScale.Crop
-                )
-            },
+            icon = R.drawable.ic_group_24,
             route = "profile"
         ),
         BottomNavItem(
-            icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_chat_24),
-                    contentDescription = "Chat"
-                )
-            },
+            icon = R.drawable.ic_chat_24,
             route = "chat"
         ),
         BottomNavItem(
-            icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_group_24),
-                    contentDescription = "Matches"
-                )
-            },
+            icon = R.drawable.ic_group_24,
             route = "matches"
         )
     )
